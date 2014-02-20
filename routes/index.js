6b456db3148d6fbc959096e5b1e7dd3d3da914f4
adam souzis <adam@souzis.com>
2014-02-20 14:37:50 -0800

    add some static content

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,39 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { debug: process.env.DEBUG });
+  res.render('index', { debug: process.env.DEBUG,
+    categories: [ 
+    {id: 1, title: "Environment",
+    activity: [
+      { avatar: "/static/images/user01.jpg",
+        itemtype: "review",
+        contents: 'EllenR reviewed <i>Build a well in Foobar</i>'
+      },
+      { avatar: "/static/images/user02.jpg",
+        itemtype: "post",
+        contents: "SamG shared a link: <a href='#'>Nebraska judge calls law that let governor approve Keystone XL pipeline route unconstitutional</a>"
+      },
+      { avatar: "/static/images/user01.jpg",
+        itemtype: "nomination",
+        contents: 'Sarah endorsed <i>Build a well in Foobar</i>'
+      },
+     /* { avatar: "",
+        itemtype: "comment",
+        contents: ""
+      },*/
+    ],
+    topProposal: {
+      title: "tusk.org",
+      heroimage:"/static/images/tusk-org.jpg",
+      endorsements: 128
+    }
+    
+    },
+    {title: "Human Rights",
+    activity: [],
+    },
+    {title: "Education",activity: []},
+    {title: "Internet Freedoms",activity: []},
+    ]
+    });
 };
