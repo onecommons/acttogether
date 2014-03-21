113139be718a72de81c4690ba5ef00ba69ce848c
adam souzis <adam@souzis.com>
2014-03-21 02:39:29 -0700

    geek out on jsonrpc unit tests

--- test/jsonrpctest.js
+++ test/jsonrpctest.js
@@ -3,15 +3,27 @@ var express = require('express')
   , jsonrpc = require('jsonrpc');
 
 describe('jsonrpc', function(){
+  var app = express();
+  app.use(express.bodyParser());
+
   describe('.router', function(){
       var app = express();
       app.use(express.bodyParser());
 
       app.post('/', function (req, res, next) {
         jsonrpc.router(req, res, next, {
+          noparams: function(params, respond) {
+            respond({result: "hello"})
+          },
+          ping: function (params, respond) {
+            respond({ result: ["hello", params] });
+          },
+          error: function (params, respond) {
+            respond( jsonrpc.INTERNAL_ERROR );
+          },
           get_data: function (params, respond) {
             respond({ result: ["hello", 5] });
-          } 
+          }
         });
       });
 
@@ -19,16 +31,26 @@ describe('jsonrpc', function(){
       request(app)
       .post('/')
       //.set('Content-Type', 'application/json') //unnecessary since its the default
-      .send({"jsonrpc":"2.0","method":"get_data","id":8})
-      .expect('{"jsonrpc":"2.0","id":8,"result":["hello",5]}', done);
+      .send({"jsonrpc":"2.0","method":"ping","id":8})
+      .expect('{"jsonrpc":"2.0","id":8,"result":["hello",null]}', done);
+    });
+
+    it('should handle internal errors correctly', function(done){
+      request(app)
+      .post('/')
+      //.set('Content-Type', 'application/json') //unnecessary since its the default
+      .send({"jsonrpc":"2.0","method":"error","id":8})
+      .expect( '{"jsonrpc":"2.0","id":8,"error":{"code":-32603,"message":"Internal error"}}', done);
     });
 
     it('should handle batch requests', function(done){
       request(app)
       .post('/')
       //.set('Content-Type', 'application/json') //unnecessary since its the default
-      .send([{"jsonrpc":"2.0","method":"get_data","id":9}, {"jsonrpc":"2.0","method":"get_data"}, {"jsonrpc":"2.0","method":"get_data","id":10}])
-      .expect('[{"jsonrpc":"2.0","id":9,"result":["hello",5]},{"jsonrpc":"2.0","id":10,"result":["hello",5]}]', done);
+      .send([{"jsonrpc":"2.0","method":"ping",params:["foo"],"id":9}, {"jsonrpc":"2.0","method":"get_data"}, {"jsonrpc":"2.0","method":"ping",params:{named:1},"id":10}, {"jsonrpc":"2.0","method":"noparams","id":11}])
+      .expect(
+ '[{"jsonrpc":"2.0","id":9,"result":["hello",["foo"]]},{"jsonrpc":"2.0","id":10,"result":["hello",{"named":1}]},{"jsonrpc":"2.0","id":11,"result":"hello"}]'
+        , done);
     });
 
     it('should send an empty response to a notification', function(done){
@@ -38,6 +60,6 @@ describe('jsonrpc', function(){
       .send({"jsonrpc":"2.0","method":"get_data"})
       .expect('', done);
     });
-
   });
+
 })
