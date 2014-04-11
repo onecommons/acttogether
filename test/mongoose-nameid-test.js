bd178cd7e49127174a30098233e37e5ab810d74b
Tim Perkis <tim@perkis.com>
2014-04-10 17:21:28 -0700

    fix tests, add open.js

--- test/mongoose-nameid-test.js
+++ test/mongoose-nameid-test.js
@@ -8,7 +8,6 @@ var mongoose = require('mongoose');
 var util = require('util');
 var nid = require('../lib/mongoose-nameid');
   
-var db = mongoose.connect('mongodb://localhost/db');
 
 // variation one: our schema creation, standard model creation
 /*
@@ -32,10 +31,20 @@ function rndString(len)
 }
 
 
+var db;
 
 // variation two: standard schema cration, our model creation.
 describe('schema name ids', function() {
-    it('should write/read records with ids we like', function(){
+    
+    before(function(){
+       db = mongoose.connect('mongodb://localhost/db');    
+    });
+    
+    after(function(){
+        mongoose.connection.close();
+    });
+    
+    it('should write/read records with ids we like', function(done){
         var Item = nid.createSchema( 'Item',{
             first_name: String,
             last_name: String,
@@ -52,15 +61,19 @@ describe('schema name ids', function() {
        record.save(function (err) {
     
         ItemModel.find({}, function(err, users) {
-
+        
+        var passed = false;
         for (var i=0, counter=users.length; i < counter; i++) {
 
           var user = users[i];
 
-          console.log( "Item => _id: " + user._id + ", first_name: " + user.first_name + ", last_name: " + user.last_name );
+         //  console.log( "Item => _id: " + user._id + ", first_name: " + user.first_name + ", last_name: " + user.last_name );
 
         }
-
+        
+       //  mongoose.connection.close();
+        done();
+        
       });
 
    });
