9e5d0b1330c1d4d290019072dee10f93e4c2b095
adam souzis <adam@souzis.com>
2014-04-17 21:18:26 -0700

    get about/index-old.html working

--- routes/index.js
+++ routes/index.js
@@ -68,7 +68,12 @@ module.exports = function(app, passport) {
 
   // Home Page
   app.get('/', function(req, res) {
-    res.render('index', getVars({
+    res.render('index', getVars());
+  });
+
+  // Render 'about' static pages
+  app.get('/about/:pagename', function(req, res) {
+    res.render(req.params.pagename, getVars({
       categories: [{
         id: 1,
         title: "Environment",
@@ -107,11 +112,6 @@ module.exports = function(app, passport) {
         activity: []
       }, ]
     }));
-  });
-
-  // Render 'about' static pages
-  app.get('/about/:pagename', function(req, res) {
-    res.render(req.params.pagename, getVars());
   });
 
   //
