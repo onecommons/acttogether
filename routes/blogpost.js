1fc0aabf4e96a271fa1d350b7799c4d04a474794
Tim Perkis <tim@perkis.com>
2014-04-26 09:19:54 -0700

    fix merge error from greg's reorg of routes

--- routes/blogpost.js
+++ routes/blogpost.js
@@ -11,30 +11,45 @@ module.exports = function(req, res) {
       _id: '@user@100'
     }; // vile hack TRP fake current user info. 
 
-  Item
-    .find({
-      parent: '@post@' + req.params.id
-    })
-    .populate('creator')
-    .exec(function(err, Items) {
-      if (err) {
-        console.log("MONGOOSE EXEC ERROR", err);
-      }
-      for (var i = 0, n = Items.length; i < n; i++) {
-        if (Items[i].__t === 'Post') {
-          postItem = Items[i];
-        } else {
-          commentItems.push(Items[i]);
-        }
-      }
-      // console.log(postItem, commentItems);
-      var datestr = moment(postItem.modDate).format("MMMM DD YYYY");
-      res.render('blogpost.html', {
-        post: postItem,
-        post_last_edit: datestr,
-        comments: commentItems,
-        user: theUser // vile hack TRP to keep going.
-      });
+  //
+  // post detail
+  //
+  // get the post and comments.
+  var postItem = {}
+    , commentItems = []
+    , theUser = { _id: '@user@100'}; // vile hack TRP fake current user info. 
 
-    });
+  Item
+    .find({ parent: '@post@'+req.params.id})
+    .sort({modDate: 'desc'})
+    .populate('creator')
+    .exec(function(err,Items) {
+      if(err) { console.log("MONGOOSE EXEC ERROR", err);}
+      for(var i=0, n=Items.length; i < n; i++){
+
+          switch(Items[i].__t) {
+            case 'Post':
+              postItem = Items[i];
+              break;
+
+            case 'Comment':
+              commentItems.push(Items[i]);
+              Items[i].ago = moment(Items[i].modDate).fromNow();
+              break;
+
+            /* other item types? NIY */
+          }
+      }
+       // console.log(postItem, commentItems);
+       var datestr = moment(postItem.modDate).format( "MMMM DD YYYY");
+       res.render('blogpost.html', {
+         messages: req.flash('info'),
+         post: postItem,
+         post_last_edit: datestr,
+         comments: commentItems,
+         user: theUser  // vile hack TRP to keep going.
+     });
+
+  }); // exec()
+
 }
