231c68ceca5a3ba779083dc7eb4f21174c59b445
adam souzis <adam@souzis.com>
2014-05-29 17:29:52 -0700

    using nojournal option causes test "datastore .mongoose should not create a doc with existing id" to fail

--- test/config/app.js
+++ test/config/app.js
@@ -1,5 +1,5 @@
 module.exports = {
-    dburl: "mongodb://127.0.0.1:27017/ocdemotest?journal=false",
+    dburl: "mongodb://127.0.0.1:27017/ocdemotest",
     port: null, //don't specify a port so we can let the OS choose a free one
     autoUpdates: false,
 }
