9cff9fba0ec62601edae632208eb7664bf67c631
adam souzis <adam@souzis.com>
2014-06-12 12:19:10 -0700

    enable jsonrpc requests to depend on each other

--- test/jsonrpctest.js
+++ test/jsonrpctest.js
@@ -16,16 +16,28 @@ describe('jsonrpc', function(){
             respond({result: "hello"})
           },
           ping: function (params, respond) {
-            respond({ result: ["hello", params] });
+            respond({ result: ["hello", params]});
+          },
+          ping_number: function (params, respond) {
+            respond({ result:params});
           },
           error: function (params, respond) {
             respond( jsonrpc.INTERNAL_ERROR );
           },
           get_data: function (params, respond) {
             respond({ result: ["hello", 5] });
+          },
+          dependant_method: function (params, respond, promises) {
+            promises.forEach(function(p) {
+              if (p.request.method == 'get_data') {
+                p.then(function(result){
+                  respond({result: result.result[1]+1});
+                });
+              }
+            });
           }
         });
-      });
+     });
 
     it('should route a jsonrpc method', function(done){
       request(app)
@@ -33,6 +45,14 @@ describe('jsonrpc', function(){
       //.set('Content-Type', 'application/json') //unnecessary since its the default
       .send({"jsonrpc":"2.0","method":"ping","id":8})
       .expect('{"jsonrpc":"2.0","id":8,"result":["hello",null]}', done);
+    });
+
+    it('should route a handle false-y looking params correctly', function(done){
+      request(app)
+      .post('/')
+      //.set('Content-Type', 'application/json') //unnecessary since its the default
+      .send({"jsonrpc":"2.0","method":"ping_number", "params":0, "id":8})
+      .expect('{"jsonrpc":"2.0","id":8,"result":0}', done);
     });
 
     it('should handle internal errors correctly', function(done){
@@ -54,7 +74,6 @@ describe('jsonrpc', function(){
     it('should handle batch requests', function(done){
       request(app)
       .post('/')
-      //.set('Content-Type', 'application/json') //unnecessary since its the default
       .send([{"jsonrpc":"2.0","method":"ping",params:["foo"],"id":9}, {"jsonrpc":"2.0","method":"get_data"}, {"jsonrpc":"2.0","method":"ping",params:{named:1},"id":10}, {"jsonrpc":"2.0","method":"noparams","id":11}])
       .expect(
  '[{"jsonrpc":"2.0","id":9,"result":["hello",["foo"]]},{"jsonrpc":"2.0","id":10,"result":["hello",{"named":1}]},{"jsonrpc":"2.0","id":11,"result":"hello"}]'
@@ -76,6 +95,24 @@ describe('jsonrpc', function(){
       .send({"jsonrpc":"2.0","method":"doesntexist"})
       .expect('', done);
     });
+
+    it('should handle dependant methods', function(done){
+      request(app)
+      .post('/')
+      .send([{"jsonrpc":"2.0","method":"get_data", "id":9}, {"jsonrpc":"2.0","method":"dependant_method","id":11}])
+      .expect(
+    '[{"jsonrpc":"2.0","id":9,"result":["hello",5]},{"jsonrpc":"2.0","id":11,"result":6}]'
+        , done);
+    });
+
+    it('should timeout with dependant methods without the dependant', function(done){
+      var timeout = setTimeout(done, 50); //expect this test to timeout
+      request(app)
+      .post('/')
+      .send({"jsonrpc":"2.0","method":"dependant_method","id":11})
+      .expect('should never get a response', done);
+    });
+
 
   });
 
