ed018e0726c680eacad83eec67c3e59051c1ab3f
adam souzis <adam@souzis.com>
2014-06-15 16:41:56 -0700

    add a couple of more config settings

--- config/app.js
+++ config/app.js
@@ -2,4 +2,6 @@ module.exports = {
     dburl: "mongodb://127.0.0.1:27017/ocdemo",
     port: 3000,
     autoUpdates: true,
+    //cookie_secret: 'your secret here',
+    //sessionfactory: function() {return {store: new FileStore()}},
 }
